e5fa6fef91eec658a6a9a27e545a04965eac105b
luckyxuanyeah <liuyuxuan17@csu.ac.cn>
2018-10-10 00:00:00 +0800

    test_github_streak

--- PAT/code2/Fourth Chapter/green.py
+++ PAT/code2/Fourth Chapter/green.py
@@ -37,4 +37,4 @@ def daily_commit(start_date, end_date):
 
 
 if __name__ == '__main__':
-    daily_commit(datetime.date(2018, 10, 14), datetime.date(2018, 10, 16))
+    daily_commit(datetime.date(2018, 10, 10), datetime.date(2018, 10, 11))
