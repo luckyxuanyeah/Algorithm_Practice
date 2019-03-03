366d116cc56d38f310def6aaf2b024bb2adb8437
Yuxuan Liu <liuyuxuan17@csu.ac.cn>
2019-03-03 22:42:58 +0800

    Runtime: 2556 ms, faster than 6.64% of C++ online submissions for Isomorphic Strings.

--- Leetcode2019/205IsomorphicStrings/answer.cpp
+++ Leetcode2019/205IsomorphicStrings/answer.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+	int len1 = s.length(), len2 = t.length();
+	if (len1 != len2)
+		return false;
+	else 
+	{
+		for (int i = 0; i < len1; i++)
+		{
+			for (int j = i+1; j < len1; j++)
+			{
+				if ((s[i] == s[j] && t[i] != t[j]) || (s[i] != s[j] && t[i] == t[j]))
+				{
+					return false;
+				}
+			}
+		}
+		
+	}
+        return true;
+    }
+};
 
+//Runtime: 2556 ms, faster than 6.64% of C++ online submissions for Isomorphic Strings.
+//Memory Usage: 8.9 MB, less than 65.79% of C++ online submissions for Isomorphic Strings.
+//Next challenges: Word Pattern
