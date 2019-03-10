0e62fdbc237dbb66a13834b777aaf590d2a19425
Yuxuan Liu <liuyuxuan17@csu.ac.cn>
2019-03-10 16:02:47 +0800

    Runtime: 8 ms, faster than 99.23% of C++ online submissions for Find Peak Element.

--- Leetcode2019/162Find Peak Element/answer.cpp
+++ Leetcode2019/162Find Peak Element/answer.cpp
@@ -55,3 +55,88 @@ int main()
 //Runtime: 8 ms, faster than 99.23% of C++ online submissions for Find Peak Element.
 //Memory Usage: 8.9 MB, less than 63.44% of C++ online submissions for Find Peak Element.
 //Next challenges:Peak Index in a Mountain Array
+
+
+// 162Find Peak Element.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include "vector"
+using namespace std;
+
+int findPeakElement(vector<int>& nums) {
+	int ans;
+	if (nums.size() == 1)
+		return nums.size() - 1;
+	int count = 0;
+	int temp = nums.size() / 2;
+	while (temp < nums.size() && temp>=0)
+	{
+		if (temp + 1 < nums.size() && temp - 1 >= 0)
+		{
+			if (nums[temp] > nums[temp + 1] && nums[temp] > nums[temp - 1])
+			{
+				ans = temp;
+				break;
+			}
+			else if(nums[temp]<nums[temp+1])
+			{
+				temp = temp + 1;
+				count++;
+				continue;
+			}
+			else if (nums[temp] < nums[temp - 1])
+			{
+				temp = temp - 1;
+				count++;
+				continue;
+			}
+		}
+		if (temp == 0)
+		{
+			if (nums[temp] > nums[temp + 1])
+			{
+				ans = temp;
+				break;
+			}
+			else if (nums[temp] < nums[temp + 1])
+			{
+				temp = temp + 1;
+				continue;
+			}
+		}
+		if (temp = nums.size() - 1)
+		{
+			if (nums[temp] > nums[temp - 1])
+			{
+				ans = temp;
+				break;
+			}
+			else if (nums[temp] < nums[temp - 1])
+			{
+				temp = temp - 1;
+			}
+		}
+	}
+	return ans;
+}
+
+int main()
+{
+	vector<int> nums;
+	nums.push_back(2);
+	nums.push_back(1);
+	/*nums.push_back(1);
+	nums.push_back(2);
+	nums.push_back(1);
+	nums.push_back(3);
+	nums.push_back(5);
+	nums.push_back(6);
+	nums.push_back(4);*/
+	int a=findPeakElement(nums);
+	cout << a << endl;
+	return 0;
+}
+//Runtime: 8 ms, faster than 99.23% of C++ online submissions for Find Peak Element.
+//Memory Usage: 9 MB, less than 36.92% of C++ online submissions for Find Peak Element.
